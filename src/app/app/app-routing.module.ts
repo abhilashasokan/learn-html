86ba38e9aa82b95c64fc3e9197727e6ab3105683
AbhilashAsokan <abhilashasokan@icloud.com>
2023-09-16 15:40:11 +0530

    :recycle: Refactor app-routing.module.ts for better readability and performance.

--- src/app/app/app-routing.module.ts
+++ src/app/app/app-routing.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/cms/overview' },
-  { path: 'cms', loadChildren: () => import('../pages/pages.module').then(m => m.PagesModule) },
-  { path: 'software', loadChildren: () => import('../software-research/software-research.module').then(m => m.SoftwareResearchModule) }
+    {path: '', pathMatch: 'full', redirectTo: '/cms/overview'},
+    {path: 'cms', loadChildren: () => import('../pages/pages.module').then(m => m.PagesModule)},
+    {path: 'software', loadChildren: () => import('../software-research/software-research.module').then(m => m.SoftwareResearchModule)}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {
+        useHash: true,
+        enableTracing: false,
+    })],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
